ed4aa77fbd800ead1aa1de16619669f69633e2b1
Ryan <ryan.hunter@techelevator.com>
2023-09-27 13:06:46 -0500

    Added vue2 && vue3 for project

---
+++ vue-3/src/services/ProductService.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+/**
+ * This service class is used to interact with the Product API.
+ * All methods return a Promise so that the calling code can handle both success and 
+ * error responses appropriately. 
+ */
+export default {
+
+  getProducts() {
+    return axios.get('/products');
+  },
+
+  searchProducts(name) {
+    // let querystring = "";
+    // if (name) {
+    //     querystring += `name=${name}`;
+    // }
+    // if (querystring) {
+    //     querystring = "?" + querystring;
+    // }
+    // return axios.get('/products' + querystring);
+    return axios.get(`/products?name=${name}`);
+  },
+
+  getProductById(productId) {
+    return axios.get(`/products/${productId}`);
+  },
+  
+}
